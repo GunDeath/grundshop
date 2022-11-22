f6d791d91c5baa4ea7d2bff25cbf8a110257665b
philipp <you@example.com>
2022-11-22 18:22:03 +0300

    remove problem with router

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
-import {useLocation} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {useLocation, useParams} from "react-router-dom";
 import classes from './ProductPage.module.css'
 import CatalogAside from "../CatalogAside/CatalogAside";
 import MyLowTitle from "../UIUX/titles/low_title/MyLowTitle";
 import RegularText from "../UIUX/body/RegularText";
 import MyBlogTitle from "../UIUX/titles/blog_title/MyBlogTitle";
 import parse from 'html-react-parser'
+import {api} from "../../woocommerce_api";
 
 const ProductPage = (props) => {
+    const params = useParams()
     const location = useLocation()
-    const { product } = location.state
-    console.log(product)
+    const [singleProduct, setSingleProduct] = useState({})
+
+
+    useEffect(()=>{
+        if(location.state !== null){
+            const { product } = location.state
+            setSingleProduct(product)
+        }else{
+            api.get(`products/${params.id}`)
+                .then(response => {if(response.status === 200){setSingleProduct(response.data)}})
+                .catch(error => {})
+        }
+    },[])
     return (
         <div className={classes.product_main__layout}>
             <CatalogAside/>
-            <div>
-                <MyLowTitle>{product.name}</MyLowTitle>
+            <div className={classes.product_cart_main}>
+                <MyLowTitle>{singleProduct.name}</MyLowTitle>
                 <br/>
-                <RegularText>{product.sku}</RegularText>
+                <RegularText>{singleProduct.sku}</RegularText>
                 <br/>
-                <MyBlogTitle title={product.price}/>
+                <MyBlogTitle title={singleProduct.price}/>
                 <br/>
-                <div className={classes.description}>{parse(product.description)}</div>
+                {singleProduct.description
+                    ? <div className={classes.description}>{parse(singleProduct.description)}</div>
+                    : <></>
+                }
+
             </div>
 
         </div>
